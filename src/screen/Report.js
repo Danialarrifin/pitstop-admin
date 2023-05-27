565801eed40712aa15c16e5c33142410c8b5b39d
danialarrifin <danialrrifin11@gmail.com>
2023-05-27 16:29:34 +0800

    delete method transactions report

--- src/screen/Report.js
+++ src/screen/Report.js
@@ -9,6 +9,31 @@ import axios from 'utils/axios'
 
 export default function Report() {
   const [transaction, setTransaction] = useState([]);
+  const [selectedItem, setSelectedItem] = useState(null);
+  const [showModal, setShowModal] = useState(false);
+  const [showDeleteModal, setShowDeleteModal] = useState(false);
+
+  const submitTransactionDeletion = async (id) => {
+    console.log(id);
+    try {
+      const response = await axios.get(
+        `/transactions/delete?transactionId=${id}`,
+        {
+          headers: {
+            'Authorization': `Bearer ${JSON.parse(localStorage.getItem('token'))}`
+          }
+        }
+      );
+      console.log('response transaction', response.data);
+      setShowDeleteModal(false);
+      setSelectedItem();
+      await getAllTransaction();
+    } catch (err) {
+      console.log(err);
+      setShowDeleteModal(false);
+      setSelectedItem();
+    }
+  };
 
   const getAllTransaction = async () => {
     try {
@@ -41,6 +66,64 @@ export default function Report() {
           <div className='p-5 w-full'>
             <h1 className='text-xl mb-2 font-bold'>Transactions Report</h1>
             <div className='overlow-auto rounded-lg shadow'>
+            {showDeleteModal ? (
+                <>
+                  <div className='fixed inset-0 z-10 overflow-y-auto'>
+                    <div
+                      className='fixed inset-0 w-full h-full bg-black opacity-40'
+                      onClick={() => {
+                        setShowDeleteModal(false);
+                      }}
+                    ></div>
+                    <div className='flex items-center min-h-screen px-4 py-8'>
+                      <div className='relative w-full max-w-lg p-4 mx-auto bg-white rounded-md shadow-lg'>
+                        <div className='mt-3 sm:flex'>
+                          <div className='flex items-center justify-center flex-none w-12 h-12 mx-auto bg-red-100 rounded-full'>
+                            <svg
+                              xmlns='http://www.w3.org/2000/svg'
+                              className='w-6 h-6 text-red-600'
+                              viewBox='0 0 20 20'
+                              fill='currentColor'
+                            >
+                              <path
+                                fillRule='evenodd'
+                                d='M8.257 3.099c.765-1.36 2.722-1.36 3.486 0l5.58 9.92c.75 1.334-.213 2.98-1.742 2.98H4.42c-1.53 0-2.493-1.646-1.743-2.98l5.58-9.92zM11 13a1 1 0 11-2 0 1 1 0 012 0zm-1-8a1 1 0 00-1 1v3a1 1 0 002 0V6a1 1 0 00-1-1z'
+                                clipRule='evenodd'
+                              />
+                            </svg>
+                          </div>
+                          <div className='mt-2 text-center sm:ml-4 sm:text-left'>
+                            <h4 className='text-lg font-medium text-gray-800'>
+                              Delete Transaction ?
+                            </h4>
+                            <p className='mt-2 text-[15px] leading-relaxed text-gray-500'>
+                              Are you sure you want to delete this vehicle?
+                            </p>
+                            <div className='items-center gap-2 mt-3 sm:flex'>
+                              <button
+                                className='w-full mt-2 p-2.5 flex-1 text-white bg-red-600 rounded-md outline-none ring-offset-2 ring-red-600 focus:ring-2'
+                                onClick={() =>
+                                  submitTransactionDeletion(selectedItem?.id)
+                                }
+                              >
+                                Delete
+                              </button>
+                              <button
+                                className='w-full mt-2 p-2.5 flex-1 text-gray-800 rounded-md outline-none border ring-offset-2 ring-indigo-600 focus:ring-2'
+                                onClick={() => {
+                                  setShowDeleteModal(false);
+                                }}
+                              >
+                                Cancel
+                              </button>
+                            </div>
+                          </div>
+                        </div>
+                      </div>
+                    </div>
+                  </div>
+                </>
+              ) : null}
               <div className='overflow-auto rounded-lg shadow hidden md:block'>
                 <table className='w-full'>
                   <thead className='bg-gray-50 border-b-2 border-gray-200'>
@@ -69,7 +152,11 @@ export default function Report() {
                       <td className='p-3 text-sm text-gray-700 whitespace-nowrap'>{item.updated_at}</td>
                       <td className='p-3 text-sm text-gray-700 whitespace-nowrap'><button class="bg-yellow-500 hover:bg-yellow-700 text-black font-bold py-2 px-4 rounded mr-3">
                           Edit
-                        </button><button class="bg-red-500 hover:bg-red-700 text-white font-bold py-2 px-4 rounded">
+                        </button><button class="bg-red-500 hover:bg-red-700 text-white font-bold py-2 px-4 rounded"
+                        onClick={() => {
+                          setShowDeleteModal(true);
+                          setSelectedItem(item);
+                        }}>
                             Delete
                           </button></td>
                     </tr>
